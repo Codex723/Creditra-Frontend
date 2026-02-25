df03de7459b7c2bf41821c172ba51fb9ce8a3150
legend4tech <ajuludennis14@gmail.com>
2026-02-25 08:10:14 +0100

    feat: Draw Credit Page Flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { Dashboard } from './pages/Dashboard';
-import { CreditLines } from './pages/CreditLines';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
+import { CreditLines } from "./pages/CreditLines";
+import DrawCreditPage from "./pages/DrawCreditPage";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <header className="header">
-          <Link to="/" className="logo">Creditra</Link>
+          <Link to="/" className="logo">
+            Creditra
+          </Link>
           <nav>
             <Link to="/">Dashboard</Link>
             <Link to="/credit-lines">Credit Lines</Link>
@@ -17,6 +20,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/credit-lines" element={<CreditLines />} />
+            <Route path="/draw-credit" element={<DrawCreditPage />} />
           </Routes>
         </main>
       </div>
